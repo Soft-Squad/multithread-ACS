c93506341bcae54ce14dd6118c0b7b58f4111cf3
Alex <wintersalex@icloud.com>
2022-02-08 10:32:27 -0800

    Initial go file, with the ability to read the file line by line

--- m-acs.go
+++ m-acs.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	f, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		// parse the line
+		fmt.Println(scanner.Text())
+	}
+}
